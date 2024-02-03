0f614909955569de0bc09530a1c6004c84643f20
CeeJay <j.chukwuony@alustudent.com>
2024-02-03 14:52:21 +0100

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
+from models import storage_t
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+
 
 class City(BaseModel, Base):
-    """
-    City class representing a city in the system.
-
-    Attributes:
-        __tablename__ (str): Represents the table name, 'cities'.
-        name (str): Represents a column containing a string (128 characters).
-                    It can't be null.
-        state_id (str): Represents a column containing a string (60 characters).
-                        It can't be null and is a foreign key to states.id.
-    """
-
-    __tablename__ = 'cities'
-
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    """ The city class, contains state ID and name """
+    if storage_t == "db":
+        __tablename__ = 'cities'
+        state_id = Column(String(128), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        places = relationship('Place', backref='cities')
+    else:
+        state_id = ""
+        name = ""
