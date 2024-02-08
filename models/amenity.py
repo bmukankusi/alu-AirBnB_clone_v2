0d659bd546f067f6063a718f043d3c785cf25fd3
bmukankusi <b.mukankusi@alustudent.com>
2024-02-08 01:07:10 -0800

    update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -3,20 +3,19 @@
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
-from models import storage_t
-from sqlalchemy import Column, String, Integer
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-    """
-    doc
-    """
+    """Representation of Amenity """
     if models.storage_t == 'db':
-        __tablename__ =- 'amenities'
+        __tablename__ = 'amenities'
         name = Column(String(128), nullable=False)
     else:
         name = ""
 
-        def __init__(self, *args, **kwargs):
-            """initializes Amenity"""
-            super().__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        """initializes Amenity"""
+        super().__init__(*args, **kwargs)
