4c98962921c4b7b0546cce00b53c794c819fc22c
bmukankusi <b.mukankusi@alustudent.com>
2024-02-08 01:00:28 -0800

    update two files

--- models/amenity.py
+++ models/amenity.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import models
 from models.base_model import BaseModel, Base
+from os import getenv
 from models import storage_t
 from sqlalchemy import Column, String, Integer
 
@@ -9,8 +11,12 @@ class Amenity(BaseModel, Base):
     """
     doc
     """
-    if storage_t == "db":
-        __tablename__ = "amenities"
+    if models.storage_t == 'db':
+        __tablename__ =- 'amenities'
         name = Column(String(128), nullable=False)
     else:
         name = ""
+
+        def __init__(self, *args, **kwargs):
+            """initializes Amenity"""
+            super().__init__(*args, **kwargs)
